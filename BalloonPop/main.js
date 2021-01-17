6b814fe1f1594980e54033b2787d028e281c12d0
NathanMQuam <nathan.m.quam@gmail.com>
2021-01-16 19:58:55 -0700

    Balloon inflates and pops

--- BalloonPop/main.js
+++ BalloonPop/main.js
@@ -1,12 +1,25 @@
 let clickCount = 0;
+let height = 120;
+let width = 100;
+let inflationRate = 20;
+let maxSize = 340;
 
 function inflate () {
     clickCount++;
 
-    var balloonElement = document.getElementById( "balloon" );
-    balloonElement.style.height = "400px";
-    balloonElement.style.width = "300px";
+    let balloonElement = document.getElementById( "balloon" );
+    height += inflationRate;
+    width += inflationRate;
+    
+    if ( height >= maxSize ) {
+        console.log( "Balloon popped!" );
+        height = 40;
+        width = 20;
+    }
 
-    var clickCountElem = document.getElementById( "click-count" );
+    balloonElement.style.height = height + "px";
+    balloonElement.style.width = width + "px";
+
+    let clickCountElem = document.getElementById( "click-count" );
     clickCountElem.innerText = clickCount.toString();
 }
