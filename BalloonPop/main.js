509605d461b08e3cede9a909e1670c25b42c0831
NathanMQuam <nathan.m.quam@gmail.com>
2021-01-16 19:48:39 -0700

    Initial Click Counter

--- BalloonPop/main.js
+++ BalloonPop/main.js
@@ -1,3 +1,12 @@
+let clickCount = 0;
+
 function inflate () {
-    console.log( "It works!" );
+    clickCount++;
+
+    var balloonElement = document.getElementById( "balloon" );
+    balloonElement.style.height = "400px";
+    balloonElement.style.width = "300px";
+
+    var clickCountElem = document.getElementById( "click-count" );
+    clickCountElem.innerText = clickCount.toString();
 }
