3bb5951804637ed52f8e62d1e861f7b671a5b255
NathanMQuam <nathan.m.quam@gmail.com>
2021-01-20 13:05:30 -0700

    Save and Load PlayersData

--- BalloonPop/main.js
+++ BalloonPop/main.js
@@ -1,13 +1,16 @@
 let startButton = document.getElementById( "start-button" );
 let inflateButton = document.getElementById( "inflate-button" );
 
+//#region GAME LOGIC AND DATA
+
 // Data
 let clickCount = 0;
 let height = 140;
 let width = 100;
 let inflationRate = 20;
 let maxSize = 340;
-let popCount = 0;
+let highestPopCount = 0;
+let currentPopCount = 0;
 let gameLength = 5000;
 let clockId = 0;
 let timeRemaining = 0;
@@ -51,7 +54,7 @@ function inflate () {
     
     if ( height >= maxSize ) {
         console.log( "Balloon popped!" );
-        popCount++;
+        currentPopCount++;
         height = 40;
         width = 20;
     }
@@ -64,12 +67,14 @@ function draw () {
     let balloonElement = document.getElementById( "balloon" );
     let clickCountElem = document.getElementById( "click-count" );
     let popCountElem = document.getElementById( "pop-count" );
+    let highPopCountElem = document.getElementById( "high-pop-count" );
     
     balloonElement.style.height = height + "px";
     balloonElement.style.width = width + "px";
     
     clickCountElem.innerText = clickCount.toString();
-    popCountElem.innerText = popCount.toString();
+    popCountElem.innerText = currentPopCount.toString();
+    highPopCountElem.innerText = highestPopCount.toString();
 }
 
 
@@ -84,6 +89,52 @@ function stopGame () {
     height = 140;
     width = 100;
 
+    if ( currentPopCount > highestPopCount ) {
+        highestPopCount = currentPopCount;
+    }
+    
+    currentPopCount = 0;
+
     stopClock();
     draw();
+}
+
+//#endregion
+
+
+
+let players = [
+    {
+        name: "Jake",
+        topScore: 1
+    }
+];
+
+function setPlayer (event) {
+    event.preventDefault();
+    let form = event.target;
+
+    let playerName = form.playerName.value;
+
+    let currentPlayer = players.find( player => player.name == playerName );
+
+    if ( !currentPlayer ) {
+        currentPlayer = { name: playerName, topScore: 0 };
+    }
+
+    console.log( currentPlayer );
+
+    form.reset();
+}
+
+function savePlayers () {
+    window.localStorage.setItem( "players", JSON.stringify( players ) );
+}
+
+function loadPlayers () {
+    let playersData = JSON.parse( window.localStorage.getItem( "players" ) );
+    
+    if ( playersData ) {
+        players = playersData;
+    }
 }
